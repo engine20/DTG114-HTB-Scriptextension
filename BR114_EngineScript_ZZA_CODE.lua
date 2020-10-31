192bc141e11a9458b1f10be266d68061372c45e4
engine20 <59338660+engine20@users.noreply.github.com>
2020-10-31 22:23:44 +0100

    Fixed a little Typing Mistake.

--- BR114_EngineScript_ZZA_CODE.lua
+++ BR114_EngineScript_ZZA_CODE.lua
@@ -22,7 +22,7 @@
 		ZZA.Value = 1;
 		ZZA.lastValue = 1;
 		--
-		ZZA.Letters = {"a", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+		ZZA.Letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
 					   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 		destination = 1;
 		destination2 = 1;
